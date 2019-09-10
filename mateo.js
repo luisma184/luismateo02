0ec1ef08ef0eedac30c552d96cd10b0d0b210ab4
luisma184 <54677078+luisma184@users.noreply.github.com>
2019-09-10 11:08:55 -0400

    Add files via upload

---
+++ mateo.js
@@ -0,0 +1,11 @@
+
+function login(){
+    var user = document .getElementById("luis").value;
+    var pass = document.getElementById("mapo").value;
+    if(user== "123" && pass == "123" ){
+        alert("funciona");
+    }
+    else{
+        alert("no funciona");
+    }
+}
